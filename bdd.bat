3942f2731487edf036d3a742e0e2f5aba50e15f1
thackamura <61438761+thackamura@users.noreply.github.com>
2020-02-25 00:42:46 +0100

    DataBase XAMPP SAVE

---
+++ bdd.bat
@@ -0,0 +1,13 @@
+@echo off
+CD "C:\xampp\mysql\bin"
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set DateTime=%%a
+
+set Yr=%DateTime:~0,4%
+set Mon=%DateTime:~4,2%
+set Day=%DateTime:~6,2%
+set Hr=%DateTime:~8,2%
+set Min=%DateTime:~10,2%
+set Sec=%DateTime:~12,2%
+
+set BackupName= ph2_zbglefvgetvs5zn__%Yr%-%Mon%-%Day%_(%Hr%-%Min%-%Sec%)
+mysqldump --user=root --password=Lemdp --databases NomdelaBase > "C:\Users\Administrateur\Desktop\backups\%BackupName%.sql"
