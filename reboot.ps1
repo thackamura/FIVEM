89fc761a05d834b19e072a1285fe84f55f3eb0b1
thackamura <61438761+thackamura@users.noreply.github.com>
2020-02-25 00:45:37 +0100

    Add files via upload

---
+++ reboot.ps1
@@ -0,0 +1,7 @@
+﻿Stop-Process -Name "FXServer" -Force
+Stop-Process -Name "cmd" -Force
+Start-Sleep -s 30
+start C:\esx\esx.cmd
+$date = Get-Date -UFormat "%x_%T"
+$date=[datetime]::Now.ToString('s').Replace(':','-')
+echo "REBOOT REALISE AVEC SUCCES" | out-file -encoding unicode C:\Users\Administrateur\Desktop\logs\$date.txt
